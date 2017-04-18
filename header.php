d4f5eba186f062a67c01d71d34f6ca9c47f2248a
Sethisfir <micka_8@hotmail.fr>
2017-04-18 13:44:28 +0200

    init mickaël c'est dur !

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Cedric Debacq</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<!-- HEADER BARRE/TOP NAV ET PHOTO -->
+	<header>
+		<!-- BARRE DU DESSUS -->
+		<div></div>
+		<!-- BARRE DE MENU -->
+		<nav></nav>
+		<!-- IMAGE DE HEADER -->
+		<img src="#">
+	</header>
