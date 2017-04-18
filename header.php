8bc62bb5d8c6e6b535e8e75ee3266ee371c550e2
John DoeYvesCharvis <charvisyves@ymail.com>
2017-04-18 15:06:56 +0200

    finish

--- header.php
+++ header.php
@@ -5,6 +5,11 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Cedric Debacq</title>
 	<link rel="stylesheet" href="">
+
+
+<!-- Begin Cookie Consent plugin  -->
+<script type="js/javascript" src="js/cookie_info.js"></script>
+<!-- End Cookie Consent plugin -->
 </head>
 <body>
 	<!-- HEADER BARRE/TOP NAV ET PHOTO -->
