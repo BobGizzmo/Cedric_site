ef2f975f8098ef9b32866e6754e2064906ea98f2
Kevin Martinez <msi03@laposte.net>
2017-04-18 14:22:32 +0200

    implementation index

---
+++ index.php
@@ -0,0 +1,12 @@
+
+	<?php get_header(); ?>
+<div id="container">
+		<div class="container">
+		<div class="content">
+		  <?php
+		  while (have_posts()) {
+		    the_post();
+		    the_content();
+		  }?>
+</div>
+<?php get_footer(); ?>
