f2ab15f8280e52ce6932cea6ef985c29e6dc1b18
Kevin Martinez <msi03@laposte.net>
2017-04-18 16:02:36 +0200

    cookieNotice

--- header.php
+++ header.php
@@ -5,13 +5,10 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Cedric Debacq</title>
 	<link rel="stylesheet" href="">
-
-
-<!-- Begin Cookie Consent plugin  -->
-<script type="js/javascript" src="js/cookie_info.js"></script>
-<!-- End Cookie Consent plugin -->
+	<script src="<?php bloginfo('template_url');?>/js/cookie_info.js"></script>
 </head>
 <body>
+<?php session_start(); ?>
 	<!-- HEADER BARRE/TOP NAV ET PHOTO -->
 	<header>
 		<!-- BARRE DU DESSUS -->
