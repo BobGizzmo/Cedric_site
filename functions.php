31fc13a280dd95318049548dc3f8c3e9368e60ce
Sethisfir <micka_8@hotmail.fr>
2017-04-19 10:46:44 +0200

    intégration animations

--- functions.php
+++ functions.php
@@ -4,3 +4,11 @@
 register_nav_menus( array(
        'primary' => __( 'Primary Menu', 'twentyfourteen' ),
     ) );
+
+$args_img_header = array(
+    'width'         => 1920,
+    'height'        => 750,
+    'default-image' => 'http://fidestia.fr/images/bg9.jpg?crc=510355820',
+    'uploads'       => true,
+    );
+    add_theme_support( 'custom-header', $args_img_header );
--- header.php
+++ header.php
@@ -4,10 +4,10 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Cedric Debacq</title>
-	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo( 'template_url' ); ?>/normalize.css">
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
 	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-	<link href="https://fonts.googleapis.com/css?family=Raleway:600" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 </head>
 <body>
 	<!-- HEADER BARRE/TOP NAV ET PHOTO -->
@@ -22,7 +22,10 @@
 				<!-- <form method="get" id="searchform" action="<?php bloginfo('home'); ?>/"> <div> <input type="text" value="<?php the_search_query(); ?>" name="s" id="s" /> <input type="submit" id="searchsubmit" value="Chercher" /> </div> </form> -->
 			</ul>
 		</div>
-		<div id="blur-div"><img id="header-img" src="http://aacf1985.org/aacf/wp-content/themes/nature-one/images/slides/slider1.jpg"></div>
+		<!-- EFFET BLUR SOUS NAV -->
+		<div id="blur-div">
+			<img id="header-img" src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" class=" col-md-10">
+		</div>
 		<!-- BARRE DE MENU -->
 		<nav id="header-barre-nav">
 			<?php
@@ -33,11 +36,10 @@
                        'container'         => 'ul',
                        'container_class'   => '',
                        'container_id'      => '',
-                       'menu_class'        => 'primary-menu',
-                       'after' 			   => '<span class="little-line"></span>')
+                       'menu_class'        => 'primary-menu')
                    );
                ?>
 		</nav>
 		<!-- IMAGE DE HEADER -->
-		<img id="header-img" src="http://aacf1985.org/aacf/wp-content/themes/nature-one/images/slides/slider1.jpg">
+		<img id="header-img" src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" class=" col-md-10">
 	</header>
